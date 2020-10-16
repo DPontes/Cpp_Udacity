61cc4d289a2ddc45bc4759aded45cffac11c7cd7
DPontes <dpontes@protonmail.com>
2020-10-16 16:05:22 +0200

    added file for lesson 2, Chp 13

---
+++ Lesson2/lesson2-13_1.cpp
@@ -0,0 +1,19 @@
+// Create a "board" by using a 2D vector
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+using std::endl;
+
+int main() {
+	vector<vector<int>> board =    {{0, 1, 0, 0, 0, 0},
+					{0, 1, 0, 0, 0, 0},
+					{0, 1, 0, 0, 0, 0},
+					{0, 1, 0, 0, 0, 0},
+					{0, 0, 0, 0, 1, 0}};
+
+	cout << "Hello" << endl;
+
+	return 0;
+}
