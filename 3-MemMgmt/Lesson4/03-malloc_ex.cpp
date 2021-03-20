2c5fd867c69d0c3e79702879ff21bc948e568645
DPontes <dpontes@protonmail.com>
2021-03-20 11:32:59 +0100

    small exercise with malloc

---
+++ 3-MemMgmt/Lesson4/03-malloc_ex.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  // void *p = malloc(3*sizeof(int));   <- Throws an error/warning as it's not possible to know the offset of the memory block.
+  int *p = (int*)malloc(3*sizeof(int));
+  printf("address=%p, value=%d\n", p, *p);
+
+  return 0;
+}
